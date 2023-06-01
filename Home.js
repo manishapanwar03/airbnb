1c40b5db653a264edbd3c1fbacc55f527f41791a
manishapanwar03 <114734876+manishapanwar03@users.noreply.github.com>
2023-06-01 05:55:02 -0700

    Add files via upload
    intail commit

---
+++ Home.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import './App.css';
+import { IconName } from "react-icons/fa";
+const Home = () => {
+  return (
+    <>
+    <div className="headere">
+		<div className="m1"><img src="https://www.companieslogo.com/img/orig/ABNB_BIG-9ccc2025.png?t=1633511992" height="30px" width="130px" className='m6'/> </div>
+		<div className="a1">
+			<pre>       <span className="a2" ><b> Stays</b></span>	    
+                <span className="a3" >  Experiences</span>     
+                  <span className="a4 " >Online Experiences</span></pre>
+		</div>
+		<div className='m2'><img src='https://wallpaperaccess.com/full/1841318.jpg' height="50px" width="50px" align="right" className='m7'/></div>
+    </div>
+    <hr/>
+	<div className="headere1">
+	<div className='m3'>
+	<button className='m5'><img src="mono.jpeg" height="45px"/> </button>  </div>
+	<div className='m8'>
+			<select className='m9'>
+			    <option value="volvo" className='m14'>  <pre>            Anywhere</pre></option>
+			    <option value="saab">Saab</option>
+			    <option value="opel">Opel</option>
+			    <option value="audi">Audi</option>
+  			</select>
+			  <div>
+  			<button className='m49'><img src="date.jpg" height="15px" width="15px" className='m50'/>June 14 - 21</button></div>
+  			 <div className='m11'><button className='m12'>+</button>  4 guests  <button className='m13'>-</button> </div>
+		</div>
+		<div className='m16'><button className='m17'><img src="https://img.icons8.com/?size=1x&id=ZHhzJTWCl4eP&format=png" height="30px"/>
+		</button> </div>
+		</div>
+		<hr/>
+		<div  className='m18' class="container-fluid">
+			<nav>
+				<lu>
+				<li className='m19'><img src="https://img.icons8.com/?size=1x&id=AO47dbOTMV5N&format=png" height="30px" width="30px" /> <p  className='m20' >All homes</p> </li>
+				<li className='m21'><img src="https://img.icons8.com/?size=1x&id=wxbxh8Nve9IX&format=png" height="30px" width="30px"/><p className='m22' >Amazing views</p></li>
+				<li className='m23'><img src="https://img.icons8.com/?size=512&id=49608&format=png" height="30px" width="30px"/><p className='m24' >Tiny Homess</p></li>
+				<li className='m25'><img src="https://img.icons8.com/?size=1x&id=5388&format=png" height="30px" width="30px"/><p className='m26' >Chefs kitchen</p></li>
+		        <li className='m27'><img src="https://cdn-icons-png.flaticon.com/128/3562/3562699.png" height="30px" width="30px"/><p className='m28' >Surfing</p> </li>
+		        <li className='m29'><img src="https://cdn-icons-png.flaticon.com/128/887/887345.png" height="30px" width="30px"/><p className='m30' >Mansions</p></li>
+		        <li className='m31'><img src="https://cdn-icons-png.flaticon.com/128/2953/2953328.png" height="30px" width="30px"/><p className='m32' >Luxe</p> </li>
+		        <li className='m33'><img src="https://cdn-icons-png.flaticon.com/128/1054/1054194.png" height="30px" width="30px"/><p className='m34' >TreeHouse</p></li>
+				<li className='m35'><img src="https://cdn-icons-png.flaticon.com/128/3319/3319235.png" height="30px" width="30px"/><p className='m36' >Comping</p></li>
+				<li className='m37'><img src="https://cdn-icons-png.flaticon.com/128/2919/2919302.png" height="30px" width="30px"/><p className='m38' >Beach front</p></li>
+				<li className='m37'><img src="https://cdn-icons-png.flaticon.com/128/2943/2943887.png" height="30px" width="30px"/><p className='m40' >Fram</p></li>
+				<li className='m37'><img src="https://cdn-icons-png.flaticon.com/128/2943/2943887.png" height="30px" width="30px"/><p className='m42' >Castles</p></li>
+				<li className='m37'><img src="https://cdn-icons-png.flaticon.com/128/7417/7417554.png" height="30px" width="30px"/><p className='m44' >Islandss</p></li>
+				<li className='m37'><img src=" https://cdn-icons-png.flaticon.com/128/3437/3437732.png" height="30px" width="30px"/><p className='m46' >Amazing Pools</p></li>
+				<li className='m37'><img src="https://cdn-icons-png.flaticon.com/128/881/881245.png" height="30px" width="30px"/><p className='m48' >Skiing</p></li>
+
+				</lu>
+			</nav>
+		</div>
+		<div className='m51'>
+		<div id="d1" className='m52'><img src="https://images.unsplash.com/photo-1591825729269-caeb344f6df2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YWlyYm5ifGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" height="216px" className='m53'/><img id='ic2' src="https://th.bing.com/th/id/OIP.U2VnP2Y4ZVH6bfllU1vPoQHaHa?w=203&h=203&c=7&r=0&o=5&dpr=1.3&pid=1.7%27className=%27ic1%27/" height="35px" width="px" className='ic1'/><h2 id="h1">  whitefish Estate </h2><p id="i1">whitefish,Montana,Unites States</p><h3 class="r1" className='m54'>$10,000/night</h3></div>
+		<div id="d1" className='m52'><img src="https://th.bing.com/th/id/OIP.2c_tC2K18I3Y3aS_iYHesAHaE7?pid=ImgDet&rs=1" height="216px" className='m53'/><img id='ic2' src="https://th.bing.com/th/id/OIP.U2VnP2Y4ZVH6bfllU1vPoQHaHa?w=203&h=203&c=7&r=0&o=5&dpr=1.3&pid=1.7%27className=%27ic1%27/" height="35px" width="px" className='ic1'/><h2 id="h1">  Luxuxry stay  </h2><p id="i1">weston,saint james,Unites States</p><h3 class="r1">$10,000/night</h3></div>
+		<div id="d1" className='m52'><img src="https://th.bing.com/th/id/OIP.aKdZeOjJ_Au2gNmjD-hAuQHaE8?pid=ImgDet&w=206&h=137&c=7&dpr=1.3" height="216px" className='m53'/><img id='ic2' src="https://th.bing.com/th/id/OIP.U2VnP2Y4ZVH6bfllU1vPoQHaHa?w=203&h=203&c=7&r=0&o=5&dpr=1.3&pid=1.7%27className=%27ic1%27/" height="35px" width="px" className='ic1'/><h2 id="h1">  Luxuxry stay  </h2><p id="i1">whitefish,Montana,Unites Statess</p><h3 class="r1">$10,000/night</h3></div>
+		<div id="d1" className='m52'><img src="https://th.bing.com/th/id/OIP.fnc_3Tw-cirZLFblWTDgAgHaE8?pid=ImgDet&w=206&h=137&c=7&dpr=1.3" height="216px" className='m53'/><img id='ic2' src="https://th.bing.com/th/id/OIP.U2VnP2Y4ZVH6bfllU1vPoQHaHa?w=203&h=203&c=7&r=0&o=5&dpr=1.3&pid=1.7%27className=%27ic1%27/" height="35px" width="px" className='ic1'/><h2 id="h1"> whitefish Estate </h2><p id="i1">weston,saint james,Unites States</p><h3 class="r1">$10,000/night</h3></div>
+		<div id="d1" className='m52'><img src="https://th.bing.com/th/id/OIP.0w7OeJ7w3oZ0k8Lo11aYIwHaE8?w=306&h=204&c=7&r=0&o=5&dpr=1.3&pid=1.7" height="216px" className='m53'/><img id='ic2' src="https://th.bing.com/th/id/OIP.U2VnP2Y4ZVH6bfllU1vPoQHaHa?w=203&h=203&c=7&r=0&o=5&dpr=1.3&pid=1.7%27className=%27ic1%27/" height="35px" width="px" className='ic1'/><h2 id="h1">  Luxuxry stay in weston </h2><p id="i1">weston,saint james,Unites States</p><h3 class="r1">$10,000/night</h3></div>
+		<div id="d1" className='m52'><img src="https://th.bing.com/th/id/OIP.nW5KEq26P5eMfmih5fz-RgHaEK?w=283&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" height="209px" className='m53'/><img id='ic2' src="https://th.bing.com/th/id/OIP.U2VnP2Y4ZVH6bfllU1vPoQHaHa?w=203&h=203&c=7&r=0&o=5&dpr=1.3&pid=1.7%27className=%27ic1%27/" height="35px" width="px" className='ic1'/><h2 id="h1">  Luxuxry stay in weston </h2><p id="i1">whitefish,Montana,Unites States</p><h3 class="r1">$10,000/night</h3></div>
+		<div id="d1" className='m52'><img src="https://th.bing.com/th/id/OIP.8r7i-GO_JcvqNnYcBTSwNgHaEK?w=292&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" height="209px" className='m53'/><img id='ic2' src="https://th.bing.com/th/id/OIP.U2VnP2Y4ZVH6bfllU1vPoQHaHa?w=203&h=203&c=7&r=0&o=5&dpr=1.3&pid=1.7%27className=%27ic1%27/" height="35px" width="px" className='ic1'/><h2 id="h1">  whitefish Estate </h2><p id="i1">weston,saint james,Unites States</p><h3 class="r1">$10,000/night</h3></div>
+		<div id="d1" className='m52'><img src="https://th.bing.com/th/id/OIP.9W4vmpZzbgb4d6OQs4J4fgHaE8?w=272&h=182&c=7&r=0&o=5&dpr=1.3&pid=1.7" height="216px" className='m53'/><img id='ic2' src="https://th.bing.com/th/id/OIP.U2VnP2Y4ZVH6bfllU1vPoQHaHa?w=203&h=203&c=7&r=0&o=5&dpr=1.3&pid=1.7%27className=%27ic1%27/" height="35px" width="px" className='ic1'/><h2 id="h1">  Luxuxry stay in weston</h2><p id="i1">whitefish,Montana,Unites States</p><h3 class="r1">$10,000/night</h3></div>
+		<div id="d1" className='m52'><img src="https://th.bing.com/th/id/OIP.wOBwEMK0gg3phkpN09iBOwHaE7?w=265&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" height="216px" className='m53'/><img id='ic2' src="https://th.bing.com/th/id/OIP.U2VnP2Y4ZVH6bfllU1vPoQHaHa?w=203&h=203&c=7&r=0&o=5&dpr=1.3&pid=1.7%27className=%27ic1%27/" height="35px" width="px" className='ic1'/><h2 id="h1">  Luxuxry stay in weston </h2><p id="i1">weston,saint james,Unites States</p><h3 class="r1">$10,000/night</h3></div>
+		<div id="d1" className='m52'><img src="https://th.bing.com/th/id/OIP.SK-FQhlZxr8rIpjJHEs2ewHaE8?w=268&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" height="216px" className='m53'/><img id='ic2' src="https://th.bing.com/th/id/OIP.U2VnP2Y4ZVH6bfllU1vPoQHaHa?w=203&h=203&c=7&r=0&o=5&dpr=1.3&pid=1.7%27className=%27ic1%27/" height="35px" width="px" className='ic1'/><h2 id="h1"> whitefish Estate </h2><p id="i1">weston,saint james,Unites</p><h3 class="r1">$10,000/night</h3></div>
+		<div id="d1" className='m52'><img src="https://images.unsplash.com/photo-1591825729269-caeb344f6df2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YWlyYm5ifGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60" height="216px" className='m53'/><img id='ic2' src="https://th.bing.com/th/id/OIP.U2VnP2Y4ZVH6bfllU1vPoQHaHa?w=203&h=203&c=7&r=0&o=5&dpr=1.3&pid=1.7%27className=%27ic1%27/" height="35px" width="px" className='ic1'/><h2 id="h1">  whitefish,Montana</h2><p id="i1">weston,saint james,Unites States</p><h3 class="r1">$10,000/night</h3></div>
+		</div>
+		
+	</>
+  )
+}
+
+export default Home;
